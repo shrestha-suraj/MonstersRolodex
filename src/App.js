377bed5e8cc7f186e3a94ac2475904d2fbdbbf13
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-08-06 11:20:36 -0500

    Added notes.txt, starting with codebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>First commit project</h1>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <h1>First commit project</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
