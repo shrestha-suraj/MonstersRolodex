9ed80724158212fbb95d9aedaf2fbd407e43ce34
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-08-06 17:41:54 -0500

    Updates

---
+++ precache-manifest.ecdd2ea648d607cea03f7dc7958eeb34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9b92075e7cb28ea4a78ce3ab84e378",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "1b473ffb22912d669a58",
+    "url": "/MonstersRolodex/static/css/main.b9335143.chunk.css"
+  },
+  {
+    "revision": "0c93387514e954bdf57e",
+    "url": "/MonstersRolodex/static/js/2.605ea4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.605ea4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b473ffb22912d669a58",
+    "url": "/MonstersRolodex/static/js/main.61a88cf7.chunk.js"
+  },
+  {
+    "revision": "425f1fd85b5840b582ed",
+    "url": "/MonstersRolodex/static/js/runtime-main.2b75c539.js"
+  }
+]);
